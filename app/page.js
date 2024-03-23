04f69724b3be064a6cf355b1b04d4c479c14c3d3
FlaSH514 <amitbabaninayak@gmail.com>
2024-03-23 09:52:14 +0530

    Authentication added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="">hi</div>;
+  return (
+    <div className="">
+      hhjhh
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
