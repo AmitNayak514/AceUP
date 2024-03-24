8092910983d8e98162e0e1c4bc3d2afac4cc080d
FlaSH514 <amitbabaninayak@gmail.com>
2024-03-24 16:58:00 +0530

    Firebase

--- firebase.config.js
+++ firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,10 +11,9 @@ const firebaseConfig = {
   projectId: "aceup-6a427",
   storageBucket: "aceup-6a427.appspot.com",
   messagingSenderId: "89832305870",
-  appId: "1:89832305870:web:b08dae5e3fb61bc9874c9f"
+  appId: "1:89832305870:web:b08dae5e3fb61bc9874c9f",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
